0beb38da3ca45b20a88f12d4d5b237d311a1a48d
Antonio Jorge <160784200+DevAntonioJorge@users.noreply.github.com>
2026-03-24 15:17:00 -0300

    Adicionar função de rebalanceamento AVL e com botão ON/OFF (#13)

--- src/No.java
+++ src/No.java
@@ -2,11 +2,13 @@ class No {
     int valor;
     No esquerda;
     No direita;
+    int altura;
 
 
     No(int valor) {
         this.valor = valor;
         this.esquerda = null;
         this.direita = null;
+        this.altura = 0;
     }
 }
